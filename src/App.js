d3ddfc3545ca3993ceacd57f2ec80a1aa9f870c9
Rodrigo Sotolani <rsotolani@gmail.com>
2022-11-09 18:39:57 -0300

    iteracoes 1, 2 e 3 completas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { Card, Row, Col, Divider, Input, Button } from 'antd';
+import foodsJSON from './foods.json';
+import FoodBox from './components/FoodBox';
+import { useState } from "react"; 
+
 
 function App() {
+
+  const [foods, setFoods] = useState(foodsJSON);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/* iteration 1
+      <h1>Iteration 1</h1>
+      <Divider>Food List</Divider>
+      <Col style={{ width: '100%', justifyContent: 'center' }}>
+        {foods.map(function (food) {
+          return (
+            <div>
+              <p>{food.name}</p>
+              <img src={food.image} alt={food.name} width={150} />
+            </div>
+          )
+        })}
+      </Col> */}
+
+      {/* iteration 2
+      <h1>Iteration 2</h1>
+      <Divider>Food Box</Divider>
+      <FoodBox food={ {
+        name: "Orange",
+        calories: 85,
+        image: "https://i.imgur.com/abKGOcv.jpg",
+        servings: 1
+      } } /> */}
+
+      {/* iteration 3*/}
+      
+      <h1>Iteration 3</h1>
+      <Divider>Food List</Divider>
+      <Col style={{ width: '100%', justifyContent: 'center' }}>
+        <div style={ {display: "flex", flexWrap: "wrap" }}>
+          {foods.map( (f) => {
+            return (
+              <FoodBox food={ {
+                name: f.name,
+                calories: f.calories,
+                image: f.image,
+                servings: f.servings
+              } } />
+            )
+            })}
+        </div>
+      </Col>
+
     </div>
   );
 }
